77f509c1f5a09127d7373c9b18692e761315d3ad
Miro Stychlok <stychlok@googlemail.com>
2026-05-16 07:15:52 +0200

    bump version and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.3.0
+
+### Features
+- Added Ambient Mode support for Always-on displays (OLED-friendly UI and battery savings)
+
 ## 0.2.0
 
 ### Features
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ncaaplayclock.referee"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.3.0"
     }
 
     compileOptions {
